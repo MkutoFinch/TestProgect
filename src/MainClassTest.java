cd2b703178b2f8fa9da3ca0f345717c38cbdd722
mkuteg <mkuteg@gmail.com>
2019-07-15 23:08:20 +0300

    Second commit

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -13,4 +13,14 @@ public class MainClassTest extends MainClass{
         }
     }
 
+    @Test
+
+    public void testGetClassNumber(){
+        if (mainClass.getClassNumber() > 45){
+            System.out.println("Успешно. Переменная getClassNumber больше 45-ти");
+        } else {
+            System.out.println("Тест провален. Переменная getClassNumber меньше 45-ти.");
+        }
+    }
+
 }
